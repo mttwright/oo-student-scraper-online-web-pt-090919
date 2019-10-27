ca54630278767974e205568fae43b457a2d9fca7
virtual-frame-7789 <mttwright@gmail.com>
2019-10-27 18:09:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,8 +26,7 @@ class Scraper
         student_info[:twitter] = box.css("a")[0].attributes["href"].value
       elsif box.css("a")[0].attributes["href"].value.include?("linkedin")
         student_info[:linkedin] = box.css("a")[0].attributes["href"].value
-      end
-      binding.pry
+      
     end
     
   end
